f78aac82b4a97b2980b21149f563cbe9976b38ec
julianxlozano <julianxlozano@gmail.com>
2021-06-22 14:18:30 -0500

    commit tree 35643323a9ba23dfc0241525ea303b06e10ff673

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+
+import React, {Component} from "react";
+import ImageSlider from "./ImageSlider";
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+          secondsLeft: props.initialCount
+        }
+      }
+
+    bombTimer = () => {
+       return this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+    }
+
+  
+    render() {
+        return(this.bombTimer())
+    }
+} 
